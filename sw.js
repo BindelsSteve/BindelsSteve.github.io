04694a0e9e2312dfdacc36cab738c6323b85f819
SteveBindels <steve_bindels@hotmail.com>
2018-05-28 17:42:19 +0200

    idk what i'm doing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Chrome's currently missing some useful cache methods,
+/*// Chrome's currently missing some useful cache methods,
 // this polyfill adds them.
 importScripts('serviceworker-cache-polyfill.js');
 
@@ -49,5 +49,56 @@ self.addEventListener('fetch', function(event) {
       // fetch, which will use the network.
       return response || fetch(event.request);
     })
+  );
+});*/
+
+const cacheFiles = [
+  './',
+  'css/style.css',
+  'js/index.js',
+  'starwars.html',
+  'css/starwarsintro.css',
+  'manifest.json',
+  'js/script.js',
+  'icons/icon-72x72.png',
+  'icons/icon-96x96.png',
+  'icons/icon-128x128.png',
+  'icons/icon-144x144.png',
+  'icons/icon-152x152.png',
+  'icons/icon-192x192.png',
+  'icons/icon-384x384.png',
+  'icons/icon-512x512.png'];
+
+const cacheName = 'v2';
+
+self.addEventListener('install',function (e) {
+e.waitUntil(
+  caches.open(cacheName).then(function(cache){
+      console.log("ServiceWorker Caching cache files");
+      return cache.addAll(cacheFiles)
+  })
+)
+});
+
+self.addEventListener('activate', function(event) {
+  // Delete all caches that aren't named in CURRENT_CACHES.
+  // While there is only one cache in this example, the same logic will handle the case where
+  // there are multiple versioned caches.
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+      return CURRENT_CACHES[key];
+  });
+
+  event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+          return Promise.all(
+              cacheNames.map(function(cacheName) {
+                  if (expectedCacheNames.indexOf(cacheName) === -1) {
+                      // If this cache name isn't present in the array of "expected" cache names, then delete it.
+                      console.log('Deleting out of date cache:', cacheName);
+                      return caches.delete(cacheName);
+                  }
+              })
+          );
+      })
   );
 });
